5a4bc593ff7c2333508c546d06c407d9983f00e4
sanjithudupa <31494156+sanjithudupa@users.noreply.github.com>
2020-06-21 19:43:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -145,6 +145,7 @@ def download_file():
     # print(request.form["file_name"])
     x = send_from_directory(app.config["OUTPUT_FOLDER"], request.form["file_name"], as_attachment=True)
     # os.remove(app.config["OUTPUT_FOLDER"] + "/" + request.form["file_name"])
+    print(app.config["OUTPUT_FOLDER"])
     print(os.system("ls " + app.config["OUTPUT_FOLDER"]))
     return x
 
