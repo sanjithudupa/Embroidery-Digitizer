5bcf19d00fa32e1a671050c4820446e7a7c685bc
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-05-05 21:34:33 -0400

    Setup layout for upload with all the proper inputs.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Footer: React.FC = () => {
+const Footer: React.FC<{sticky?: boolean}> = ({sticky}) => {
     return (
-        <footer style={{textAlign: "center", position: "absolute", bottom: "5px", width: "100%", color: "gray"}}>
+        <footer style={{textAlign: "center", position: sticky ? "sticky" : "absolute", bottom: "5px", width: "100%", color: "gray"}}>
             This project is <a href="https://github.com/sanjithudupa/embroidery-digitizer">Open Source.</a>
             <br/>
             <span>&#169;</span> 2021 <a href="https://youtube.com/sanjithar">Sanjith Udupa</a>, All Rights Reserved.
--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -1,11 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
 import Dropzone from "../components/Dropzone";
+import Footer from "../components/Footer";
 
 const Upload: React.FC = () => {
+
+    const extensions = [
+        "PES",
+        "PEC",
+        "DST",
+        "EXP",
+        "JST",
+        "VP3",
+        "U01"
+    ]
+
+    const [dropdown, setDropdown] = useState("Output File Type");
+    const [value, setValue] = useState("Yes");
+
     return (
-        <div>
-            <h1>Upload</h1>
-            <Dropzone />
+        <div style={{height: "100%", overflow: "hidden"}}>
+            <div style={{textAlign: "center", height: "99%", overflow: "auto"}}>
+                <br />
+                <h2>Let's start digitizing!</h2>
+                <br />
+
+                <h6>First, <strong>upload</strong> an SVG image file.</h6>
+                <br />
+
+                <Dropzone />
+
+                <br />
+
+                <h6>Now, choose your <strong>output file type.</strong></h6>
+                <a href="#">Not sure which type to choose?</a>
+
+                <br />
+                <br />
+
+                <Dropdown>
+                    <Dropdown.Toggle variant="info">
+                        {dropdown}
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        {
+                            extensions.map((extension) => {
+                                return <Dropdown.Item onClick={() => setDropdown(extension)}>{extension}</Dropdown.Item>
+                            })
+                        }
+                    </Dropdown.Menu>
+
+                </Dropdown>
+
+                <br />
+                <br />
+
+                Great, would you like your file to be <strong>filled?</strong>
+                <br />
+                <a href="#">Show me what this means.</a>
+
+                <br />
+                <br />
+
+                <ToggleButtonGroup
+                    name="value"
+                    type="radio"
+                    value={value}
+                    onChange={(value) => setValue(value)}
+                >
+                    <ToggleButton value={"Yes"} variant="secondary">Yes</ToggleButton>
+                    <ToggleButton value={"No"} variant="secondary">No</ToggleButton>
+                </ToggleButtonGroup>
+
+                <br />
+                <br />
+
+                Now, start the digitzation process!
+                <br />
+                <i>This may take up to two minutes</i>
+
+                <br />
+                <br />
+
+                <Button variant="success">
+                    Digitize!
+                </Button>
+
+                <br />
+
+                h 
+                
+
+            </div>
+            <br />
+            <br />
+            <Footer sticky />
         </div>
     )
 }
