99729f5ff475d929801404dd7667e66d479cd316
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-05-06 18:38:44 -0400

    Try to make upload animation

--- frontend/src/components/Dropzone.tsx
+++ frontend/src/components/Dropzone.tsx
@@ -14,10 +14,26 @@ const Dropzone: React.FC<{setFile?: Function}> = ({setFile}) => {
     const [reading, setReading] = useState(false);
     const [readingProgress, setReadingProgress] = useState(0);
 
+    // const FPS = 30;
+    // const [translate, setTranslate] = useState(0);
+    // const [scale, setScale] = useState(1);
+    
     useEffect(() => {
         file_drop_div = document.getElementById("file-drop-div") as HTMLDivElement;
         image = document.getElementById("image") as HTMLInputElement;
         upload_help_label = document.getElementById("upload-help-label") as HTMLLabelElement;
+
+        // let frame = 0;
+
+        // setInterval(() => {
+        //     if (frame >= 600)
+        //         frame = 0
+            
+        //     setTranslate(Math.sin(frame/100));
+        //     setScale(-0.6 * Math.sin(frame));
+            
+        //     frame++;
+        // }, 1000/FPS);
     }, []);
 
     function fileDragged() {
@@ -71,9 +87,23 @@ const Dropzone: React.FC<{setFile?: Function}> = ({setFile}) => {
                 
                 <div id="svg-container">
                 <svg id="upload-icon" className="bi bi-upload" width="10%" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M.5 8a.5.5 0 0 1 .5.5V12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8.5a.5.5 0 0 1 1 0V12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8.5A.5.5 0 0 1 .5 8zM5 4.854a.5.5 0 0 0 .707 0L8 2.56l2.293 2.293A.5.5 0 1 0 11 4.146L8.354 1.5a.5.5 0 0 0-.708 0L5 4.146a.5.5 0 0 0 0 .708z"/>
-                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0v-8A.5.5 0 0 1 8 2z"/>
+                    <g transform={`translate(0, ${0}) scale(1, ${1})`}>
+                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0v-8A.5.5 0 0 1 8 2z"/>
+                        <path d="M5 4.854a.5.5 0 0 0 .707 0L8 2.56l2.293 2.293A.5.5 0 1 0 11 4.146L8.354 1.5a.5.5 0 0 0-.708 0L5 4.146a.5.5 0 0 0 0 .708z" />
+                    </g>
+                    <path fill-rule="evenodd" d="M.5 8a.5.5 0 0 1 .5.5V12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8.5a.5.5 0 0 1 1 0V12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8.5A.5.5 0 0 1 .5 8z"/>
                 </svg>
+
+                {/* <svg id="upload-icon" className="bi bi-upload" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" width="15%" fill="#000000">
+                    <g>
+                        <rect fill="none" height="24" width="24"/>
+                    </g>
+                    <g>
+                        <path strokeWidth="5" d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M7,9l1.41,1.41L11,7.83V16h2V7.83l2.59,2.58L17,9l-5-5L7,9z"/>
+                    </g>
+                </svg> */}
+
+
                 </div>
                 
                 <div id="help-label-container" style={{textAlign: "center", marginTop: `-${reading ? 80 : 50}px`, paddingBottom: "px"}}>
